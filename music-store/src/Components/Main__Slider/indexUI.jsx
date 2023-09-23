6f1b3aad3ea16df9e5507784156e22ccf13f25e5
N40M10 <N40M10@disroot.org>
2023-09-23 16:27:01 -0600

    Actualizacion music-store, se arregla el slider, 2023-09-20

--- music-store/src/Components/Main__Slider/indexUI.jsx
+++ music-store/src/Components/Main__Slider/indexUI.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import ImgSliderSm from "@images/img_slide_sm.png";
+import ImgSliderMd from "@images/img_slide_md.png";
+import ImgSliderLg from "@images/img_slide_lg.png";
 import './index.scss'
 
 const Main__SliderUI = () => {
@@ -6,6 +9,11 @@ const Main__SliderUI = () => {
     <>
       <div className="main__slider">
           <h2>Fullfilling dreams with strings attached</h2>
+          <picture>
+            <source media="(min-width: 1440px)" srcSet={ImgSliderLg} />
+            <source media="(min-width: 1024px)" srcSet={ImgSliderMd} />
+            <img src={ImgSliderSm} alt="Slider Images"/>
+          </picture>
       </div>
     </>
   )
